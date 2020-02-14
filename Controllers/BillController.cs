fe4cd16eba07b1a50d314b816c52ad3ac635f3ed
Tim Fleming <tfleming26@hotmail.com>
2020-02-14 17:23:14 -0600

    Create bill controller boilerplate

---
+++ Controllers/BillController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Billafye.Controllers
+{
+  public class BillController : Controller
+  {
+    // Actions Go Here
+    public IActionResult Index()
+    {
+      // Get List of Bills from DB
+
+      // Place Bill Items into Model
+
+      // Render View Using Model
+    }
+  }
+
+}
