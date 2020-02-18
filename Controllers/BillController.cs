88c96cba9925b64b6ecc18de981d4865875bc68c
Tim Fleming <tfleming26@hotmail.com>
2020-02-17 18:47:13 -0600

    Add bill item interface to controller

--- Controllers/BillController.cs
+++ Controllers/BillController.cs
@@ -3,19 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Billafye.Services;
+using Billafye.Models;
 
 namespace Billafye.Controllers
 {
   public class BillController : Controller
   {
     // Actions Go Here
-    public IActionResult Index()
+    private readonly IBillItemService _billItemService;
+
+    public BillController(IBillItemService billItemService)
+    {
+      _billItemService = billItemService;
+    }
+    public async Task<IActionResult> Index()
     {
       // Get List of Bills from DB
+      var items = await _billItemService.GetUnpaidBillItemsAsync();
 
       // Place Bill Items into Model
+      var model = new BillafyeViewModel()
+      {
+        Items = items
+      };
 
-      // Render View Using Model
+      return View(model);
     }
   }
 
